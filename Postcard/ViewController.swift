87f2031887923559e25f30c32422f7780ecc102d
HeliNinja <georgegrauke@yahoo.com>
2015-04-28 12:25:11 -0800

    added label to display name

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 	@IBOutlet weak var enterNameTextField: UITextField!
 	@IBOutlet weak var enterMessageTextField: UITextField!
 	@IBOutlet weak var mailButton: UIButton!
+	@IBOutlet weak var nameLabel: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -26,13 +27,19 @@ class ViewController: UIViewController {
 	}
 
 	@IBAction func sendMailButtonPressed(sender: UIButton) {
-		// test commit comment
 		messageLabel.hidden = false
 		messageLabel.text = enterMessageTextField.text
 		messageLabel.textColor = UIColor.redColor()
 		
 		enterMessageTextField.text = ""
 		enterMessageTextField.resignFirstResponder()
+		
+		nameLabel.hidden = false
+		nameLabel.text = enterNameTextField.text
+		nameLabel.textColor = UIColor.blueColor()
+		
+		enterNameTextField.text = ""
+		enterNameTextField.resignFirstResponder()
 		
 		mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
 		
